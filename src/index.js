e6306ea0d03dbfd1b5bc8f550080767dbf95f4e3
Unknown <mosquitojoe85@gmail.com>
2019-08-17 17:27:39 +0800

    [ADD] 增加韓導與控制行為。

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './index.css';
 
 const gameWidth = 1920;
 const gameHeight = 1080;
+const roadPosition = { gap: 140, min: 600, max: 880 };
 
 const gameStart = {
     key: 'gameStart',
@@ -15,17 +16,33 @@ const gameStart = {
       this.load.image('cloud', 'src/assets/background/cloud_old.png');
       this.load.image('city', 'src/assets/background/city.png');
       this.load.image('road', 'src/assets/background/road.png');
+      this.load.svg('player', 'src/assets/player/player.svg', { width: 280 });
     },
     create: function(){
-      this.sky = this.add.tileSprite(gameWidth/2, 640/2, gameWidth, 640, 'sky');
-      this.cloud = this.add.tileSprite(971/2, 394/2+283, 971, 394, 'cloud');
-      this.city = this.add.tileSprite(gameWidth/2, 430/2+215, gameWidth, 430, 'city');
-      this.road = this.add.tileSprite(gameWidth/2, 460/2+619, gameWidth, 460, 'road');
+      this.sky = this.add.tileSprite(gameWidth / 2, 640 / 2, gameWidth, 640, 'sky');
+      this.cloud = this.add.tileSprite(971 / 2, 394 / 2 + 283, 971, 394, 'cloud');
+      this.city = this.add.tileSprite(gameWidth / 2, 430 / 2 + 215, gameWidth, 430, 'city');
+      this.road = this.add.tileSprite(gameWidth / 2, 460 / 2 + 619, gameWidth, 460, 'road');
+      this.player = this.add.image(300, roadPosition.min + roadPosition.gap, 'player').setInteractive();
+
+      const upAct = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+      const downAct = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+      upAct.on('down', (event) => {
+        if(this.player.y > roadPosition.min) {
+          this.player.setPosition(300, this.player.y - roadPosition.gap);
+        }
+      });
+      downAct.on('down', (event) => {
+        if(this.player.y < roadPosition.max) {
+          this.player.setPosition(300, this.player.y + roadPosition.gap);
+        }
+      });
     },
     update: function(){
       this.sky.tilePositionX += 1;
       this.city.tilePositionX += 2;
       this.road.tilePositionX += 4;
+      this.player.tilePositionX += 4;
     }
 }
 
@@ -37,25 +54,9 @@ const config = {
   height: gameHeight,
   scene: [
     gameStart,
-  ]
+  ],
 };
 
 const game = new Phaser.Game(config);
-
-// function preload() {
-//   this.load.image("logo", logoImg);
-// }
-
-// function create() {
-//   const logo = this.add.image(400, 150, "logo");
-//   this.tweens.add({
-//     targets: logo,
-//     y: 450,
-//     duration: 2000,
-//     ease: "Power2",
-//     yoyo: true,
-//     loop: -1
-//   });
-// }
 
 ReactDOM.render(<App />, document.getElementById("root"));
