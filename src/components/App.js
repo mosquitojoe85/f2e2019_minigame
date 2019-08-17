7be6ea766e18fac9ee3f419154e885e3899c3f5a
Unknown <mosquitojoe85@gmail.com>
2019-08-17 13:27:06 +0800

    [ADD] 換一個環境跟套件，改用 `Phaser`。完成移動場景。

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <main id="gameBlock" className="gameBlock"></main>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import Phaser from "phaser";
+import logoImg from "./assets/logo.png";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const gameWidth = 1920;
+const gameHeight = 1080;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const gameStart = {
+    key: 'gameStart',
+    preload: function(){
+      this.load.image('sky', 'src/assets/background/sky_old.png');
+      this.load.image('cloud', 'src/assets/background/cloud_old.png');
+      this.load.image('city', 'src/assets/background/city.png');
+      this.load.image('road', 'src/assets/background/road.png');
+    },
+    create: function(){
+      this.sky = this.add.tileSprite(gameWidth/2, 640/2, gameWidth, 640, 'sky');
+      this.cloud = this.add.tileSprite(971/2, 394/2+283, 971, 394, 'cloud');
+      this.city = this.add.tileSprite(gameWidth/2, 430/2+215, gameWidth, 430, 'city');
+      this.road = this.add.tileSprite(gameWidth/2, 460/2+619, gameWidth, 460, 'road');
+    },
+    update: function(){
+      this.sky.tilePositionX += 1;
+      this.city.tilePositionX += 2;
+      this.road.tilePositionX += 4;
+    }
+}
+
+const config = {
+  backgroundColor: '#fff',
+  type: Phaser.AUTO,
+  parent: "gameBlock",
+  width: gameWidth,
+  height: gameHeight,
+  scene: [
+    gameStart,
+  ]
+};
+
+const game = new Phaser.Game(config);
+
+// function preload() {
+//   this.load.image("logo", logoImg);
+// }
+
+// function create() {
+//   const logo = this.add.image(400, 150, "logo");
+//   this.tweens.add({
+//     targets: logo,
+//     y: 450,
+//     duration: 2000,
+//     ease: "Power2",
+//     yoyo: true,
+//     loop: -1
+//   });
+// }
+
+ReactDOM.render(<App />, document.getElementById("root"));
